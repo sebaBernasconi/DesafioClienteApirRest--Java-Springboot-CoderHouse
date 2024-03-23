f248a7ee09ed8c5a4b581c5c2cf09fa9c3ec0946
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-03-23 14:57:21 -0300

    Commit inicial

---
+++ src/main/java/com/coderhouse/desafioclienterest/DesafioClienteRestApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse.desafioclienterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioClienteRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioClienteRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderhouse/desafioclienterest/DesafioClienteRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.desafioclienterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioClienteRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
